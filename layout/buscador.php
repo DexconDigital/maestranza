47675cbb8e7483b256d6c213b97c9eee13f87866
william solano <wdsp9898@gmail.com>
2019-12-02 15:07:04 -0500

    dimenciones del boton

--- layout/buscador.php
+++ layout/buscador.php
@@ -47,7 +47,7 @@
         <div class="col-xl-3 col-md-4 col-12  margen_busca"><input type="number" class="form-control rounded-0" id="precio_minimo_buscar"  placeholder=" Precio Mínimo"></div>
         <div class="col-xl-3 col-md-4 col-12  margen_busca"><input type="number" class="form-control rounded-0" id="precio_maximo_buscar"  placeholder=" Precio Máximo"></div>
         <div class="col-xl-12 col-md-12 col-12 text-center margen_busca">
-            <button type="button" class="btn rounded-0 col-2 boton_buscar boton_dorado posicion_buscar" id="buscar"><span class="tamaño_btn_buscar">Buscar</span></button></div>
+            <button type="button" class="btn rounded-0 col-2 boton_buscar boton_dorado posicion_buscar" id="buscar" style="    padding: .70rem 2.14rem;" ><span class="tamaño_btn_buscar">Buscar</span></button></div>
 
     </div>
 </div>
